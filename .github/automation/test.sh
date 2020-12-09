4b7592a6380b5e0becf1a430c68bf1ad54f6329e
Kentaro KAWAKAMI <kawakami.k@fujitsu.com>
2020-12-09 22:27:22 +0900

    tmp

--- .github/automation/test.sh
+++ .github/automation/test.sh
@@ -61,8 +61,12 @@ fi
 CTEST_OPTS="${CTEST_OPTS} --output-on-failure"
 
 echo "CTest options: ${CTEST_OPTS}"
-cd "${BUILD_DIR}"
-ctest ${CTEST_OPTS}
+#cd "${BUILD_DIR}"
+#ctest ${CTEST_OPTS}
+cd tests/gtests
+export DNNL_VERBOSE=2
+DNNL_MAX_CPU_ISA=SSE41  ./test_s8s8s32 --gtest_filter="TestGEMM_heavy_s8s8s32_CPU/gemm_test.TestGEMM/4"
+./test_s8s8s32 --gtest_filter="TestGEMM_heavy_s8s8s32_CPU/gemm_test.TestGEMM/4"
 result=$?
 
 echo "DONE"
