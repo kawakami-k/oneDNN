c689108445c9d85d1361f2a70fb99cea331166a4
Kentaro Kawakami <kawakami.k@fujitsu.com>
2020-12-10 18:25:23 +0900

    ci: test

--- .github/automation/debug.sh
+++ .github/automation/debug.sh
@@ -62,14 +62,14 @@ CTEST_OPTS="${CTEST_OPTS} --output-on-failure"
 
 echo "CTest options: ${CTEST_OPTS}"
 cd "${BUILD_DIR}"
-ctest ${CTEST_OPTS}
+#ctest ${CTEST_OPTS}
 cd tests/gtests
 export DNNL_JIT_DUMP=1
 export DNNL_VERBOSE=2
 DNNL_MAX_CPU_ISA=SSE41  ./test_gemm_s8s8s32 --gtest_filter="TestGEMM_heavy_s8s8s32_CPU/gemm_test.TestGEMM/4"
 ./test_gemm_s8s8s32 --gtest_filter="TestGEMM_heavy_s8s8s32_CPU/gemm_test.TestGEMM/4"
 ls -l *.bin
-cd ../../../
+cd ../../../../
 tar zcfv s.tgz s/
 mv s.tgz s/
 
