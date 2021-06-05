d3f8fae1327df26836afe5bec929c9957717ccd1
Kentaro Kawakami <kawakami.k@fujitsu.com>
2021-06-05 20:24:27 +0900

    src: cpu: aarch64: eltwise: optimize relu_zero_ns fwd

--- src/cpu/aarch64/injectors/jit_uni_eltwise_injector.cpp
+++ src/cpu/aarch64/injectors/jit_uni_eltwise_injector.cpp
@@ -450,11 +450,7 @@ void jit_uni_eltwise_injector_f32<isa>::relu_compute_vector_fwd(
 template <cpu_isa_t isa>
 void jit_uni_eltwise_injector_f32<isa>::relu_zero_ns_compute_vector_fwd(
         const TRegS &vmm_src) {
-    h->mov(PRegB(IDX(p_tmp0)), h->P_ALL_ONE.b);
-    h->mov(ZRegD(IDX(z_tmp)), ZRegD(IDX(table_val(zero))));
-    h->fmaxnm(z_tmp, p_tmp0, vmm_src);
-    h->fmax(z_tmp, p_tmp0, vmm_src);
-    h->mov(ZRegD(IDX(vmm_src)), ZRegD(IDX(z_tmp)));
+    h->fmaxnm(vmm_src, p_all, 0.f);
 }
 
 template <cpu_isa_t isa>
